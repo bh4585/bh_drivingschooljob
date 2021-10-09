c8c5292e52e4696ee38dd582040b92ab4bcf0519
bh4585 <92100734+bh4585@users.noreply.github.com>
2021-10-09 05:11:02 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'adamant'
+game 'gta5'
+description 'bh drivingschooljob'
+version '1.0'
+
+server_scripts {
+	'server.lua',
+	'config.lua'
+
+}
+
+client_scripts {
+	'client.lua',
+	'config.lua'
+}
